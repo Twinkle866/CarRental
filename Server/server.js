1a95f3343c4c4a33a30acdbbdf497b7843111518
Twinkle Sharma <twinksharma18@gmail.com>
2025-08-29 04:26:33 +0530

    Added Project

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
-dotenv.config();
+dotenv.config(); 
+
+console.log("MONGODB_URI:", process.env.MONGODB_URI); // check if it prints correctly
+console.log("JWT_SECRET:", process.env.JWT_SECRET);
+console.log("IMAGEKIT_PUBLIC_KEY:", process.env.IMAGEKIT_PUBLIC_KEY);
+
+
 
 import cors from "cors";
 import connectDB from "./Configs/db.js";
@@ -10,14 +16,14 @@ import bookingRouter from "./routes/bookingRoutes.js";
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => res.send("Server is running"));
 app.use("/api/user", userRouter);
 app.use("/api/owner", ownerRouter);
-app.use("/api/bookings", bookingRouter);
+app.use("/api/bookings",bookingRouter)
 
 connectDB();
-
-export default app;   // 👈 yeh likhna zaroori hai
+export default app;
